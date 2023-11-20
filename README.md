321438c4fb608c0bcb048e2d554bc7b5233db701
thsn <tahsinerdemtr@gmail.com>
2023-11-20 18:54:36 +0300

    pubspec organized

---
+++ README.md
@@ -0,0 +1,3 @@
+# spaced_repetition_notes
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/spaced_repetition_notes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.spaced_repetition_notes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
